121edee68400070146c7f2182e290b1c0ff33bc0
Adam Malinowski <adam.malinowski@canonical.com>
2022-06-02 08:53:49 +0200

    Add tcp echo example

---
+++ examples/device/net_lwip_webserver/src/tcpecho.c
@@ -0,0 +1,168 @@
+/*
+ * Copyright (c) 2001-2003 Swedish Institute of Computer Science.
+ * All rights reserved. 
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED 
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+ * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+ * OF SUCH DAMAGE.
+ *
+ * This file is part of the lwIP TCP/IP stack.
+ * 
+ * Author: Adam Dunkels <adam@sics.se>
+ *
+ */
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "tusb.h"
+#include "lwip/opt.h"
+
+#if LWIP_NETCONN
+
+#include "lwip/sys.h"
+#include "lwip/api.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+#define TCPECHO_THREAD_PRIO  ( tskIDLE_PRIORITY + 4 )
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+static void tcpecho_thread(void *arg);
+void tcpecho_init(void);
+#if 0
+/* Private functions ---------------------------------------------------------*/
+static void tcpecho_thread(void *arg)
+{
+  struct netconn *conn, *newconn;
+  err_t err, accept_err;
+  struct netbuf *buf;
+  void *data;
+  u16_t len;
+      
+  LWIP_UNUSED_ARG(arg);
+
+  /* Create a new connection identifier. */
+  conn = netconn_new(NETCONN_TCP);
+  
+  if (conn!=NULL)
+  {  
+    /* Bind connection to well known port number 7. */
+    err = netconn_bind(conn, NULL, 7);
+    
+    if (err == ERR_OK)
+    {
+      /* Tell connection to go into listening mode. */
+      netconn_listen(conn);
+    
+      while (1) 
+      {
+        /* Grab new connection. */
+         accept_err = netconn_accept(conn, &newconn);
+    
+        /* Process the new connection. */
+        if (accept_err == ERR_OK) 
+        {
+
+          while (netconn_recv(newconn, &buf) == ERR_OK) 
+          {
+            do 
+            {
+              netbuf_data(buf, &data, &len);
+              netconn_write(newconn, data, len, NETCONN_COPY);
+          
+            } 
+            while (netbuf_next(buf) >= 0);
+          
+            netbuf_delete(buf);
+          }
+        
+          /* Close connection and discard connection identifier. */
+          netconn_close(newconn);
+          netconn_delete(newconn);
+        }
+      }
+    }
+    else
+    {
+      netconn_delete(newconn);
+    }
+  }
+}
+#endif
+/*-----------------------------------------------------------------------------------*/
+
+void tcpecho_thread(void *arg)
+{
+  struct netconn *conn, *newconn;
+  err_t err;
+  LWIP_UNUSED_ARG(arg);
+
+  /* Create a new connection identifier. */
+  conn = netconn_new(NETCONN_TCP);
+
+  /* Bind connection to well known port number 7. */
+  netconn_bind(conn, NULL, 7);
+
+  /* Tell connection to go into listening mode. */
+  netconn_listen(conn);
+
+  while (1) {
+
+    /* Grab new connection. */
+    err = netconn_accept(conn, &newconn);
+    /*printf("accepted new connection %p\n", newconn);*/
+    /* Process the new connection. */
+    if (err == ERR_OK) {
+      struct netbuf *buf;
+      void *data;
+      u16_t len;
+      
+      while ((err = netconn_recv(newconn, &buf)) == ERR_OK) {
+        /*printf("Recved\n");*/
+        do {
+             netbuf_data(buf, &data, &len);
+	     TU_LOG1("netconn_recv: %d <%s> totlen: %d\n", len, (char *)data, buf->p->tot_len);
+             err = netconn_write(newconn, data, len, NETCONN_COPY);
+#if 1
+            if (err != ERR_OK) {
+              TU_LOG1("tcpecho: netconn_write: error \"%s\"\n", lwip_strerr(err));
+            }
+#endif
+        } while (netbuf_next(buf) >= 0);
+//        netbuf_delete(buf);
+      }
+      /*printf("Got EOF, looping\n");*/ 
+      /* Close connection and discard connection identifier. */
+      netconn_close(newconn);
+      netconn_delete(newconn);
+    }
+  }
+}
+
+void tcpecho_init(void)
+{
+  sys_thread_new("tcpecho_thread", tcpecho_thread, NULL, 8192, 8);
+}
+/*-----------------------------------------------------------------------------------*/
+
+#endif /* LWIP_NETCONN */
